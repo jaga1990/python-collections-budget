738d233a4a01b87d6eb04e8eaa7e2805acd450f1
jaga1990 <agnieszka@golofit.pl>
2020-10-23 08:45:05 +0200

    formattign changes

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import collections
+import matplotlib.pyplot as plt
 
 from . import Expense
 
@@ -9,7 +9,7 @@ spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
-#spending_categories = [expense.category for expense in expenses.list] #Shorter way but due to created tests impossible to implement!
+# spending_categories = [expense.category for expense in expenses.list] #Shorter way but due to created tests impossible to implement!
 spending_counter = collections.Counter(spending_categories)
 
 top5 = spending_counter.most_common(5)
@@ -19,4 +19,3 @@ fig, ax = plt.subplots()
 ax.bar(categories, count)
 ax.set_title('# of Purchases by Category')
 plt.show()
-
